aa877af2e312b6fa5fb0f419660e9aa21bf78361
Alex Williams <alexwilliams57@hotmail.com>
2020-10-25 11:21:16 +0000

    Initial commit - project created

---
+++ FundAppsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundAppsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
